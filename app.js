ed6df8f12b3f956226729082bc1b47a23b960aa1
Sunghyun <anaclumos@gmail.com>
2018-08-06 20:16:22 +0900

    aws hosted

--- app.js
+++ app.js
@@ -16,42 +16,60 @@ app.get('/keyboard', function(req, res) {
 });
 
 app.post('/message', function(req, res) {
-	var message = req.body.content;
-	console.log('전달된 메시지입니다: ' + message);
+	console.log(req)
+	//var message = req.body.content;
+	//console.log('전달된 메시지입니다: ' + message);
 
-	var respond = {};
-
+	var msg = {
+		'message': {
+			'text' : '기본 응답'
+			}
+		};
+		/*
 	switch(message) {
 		case '선택 1':
-			respond = {
+			msg = {
 				'message': {
 					'text' : '선택 1을 선택하셨네요.'
 				}
 			}
 			break;
 		case '선택 2':
-			respond = {
+			msg = {
 				'message': {
 					'text' : '선택 2을 선택하셨네요.'
 				}
 			}
 			break;
 		case '선택 3':
-			respond = {
+			msg = {
 				'message': {
 					'text' : '선택 3을 선택하셨네요.'
 				}
 			}
 			break;
 		default:
-			respond = {
+			msg = {
 				'message': {
 					'text' : '알 수 없는 명령'
 				}
 			}
 			break;
 	}
-	res.json(respond);
+    */
+	res.json(msg);
+
+	app.get('/keyboard', function(req, res) {
+	var data = {
+  'type': 'buttons',
+  'buttons': [
+    '선택 1',
+    '선택 2',
+    '선택 3'
+  	]
+	}
+	res.json(data);
+	});
 })
 
 http.createServer(app).listen(8081, '0.0.0.0');
