2c24a823613c35591063700c3f6f2454c2eca4af
pi <anaclumos@icloud.com>
2018-08-06 18:00:38 +0900

    shovel

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ var app = express();
 
 app.get('/keyboard', function(req, res) {
 	var data = {
-		'type': 'buttons',
-		'buttons': ['Fruits', 'Vegetables', 'Information']
+		"type": "buttons",
+		"buttons": ["Fruits", "Vegetables", "Information"]
 	};
+
 	res.json(data);
 });
 
-http.createServer(app).listen(9090, function() {
-	console.log('server up and running...')
-});
+http.createServer(app).listen(9090, "0.0.0.0");
+
+console.log('server up and running at http://0.0.0.0:8080')
