394a4151b9553c66db7d6c5ba43e3522baaa4bea
Sunghyun <anaclumos@gmail.com>
2018-09-04 20:10:05 +0900

    pythonshell fixed

--- app.js
+++ app.js
@@ -36,11 +36,13 @@ app.post("/message",function (req, res) {
         type     : req.body.type,
         content  : req.body.content
     };
+
     console.log(_obj.user_key + "가 " + _obj.type + "로 된, \"" + _obj.content + "\"라는 문자를 보냈습니다.");
     var bionic = new PythonShell('Bionic.py');
     pyshell.on('_obj.content', function (message) {
       res.set({"content-type": "application/json"}).send(JSON.stringify(message));
     });
+});
 
 http.createServer(app).listen(8081, "0.0.0.0");
 console.log("Fairy up and running at http://0.0.0.0:8081")
